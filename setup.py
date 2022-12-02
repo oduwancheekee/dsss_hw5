9af8797379b901b02941fd8c4efabb1fb89784c5
Ivan Timofeev <oduwancheekee@gmail.com>
2022-12-02 18:10:08 +0100

    [feat] made installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dsss_hw5_snowflake',
+    version='0.1.0',
+    author='Ivan Timofeev',
+    author_email='ivan.timofeev@fau.de',
+    packages=find_packages(),
+    requires=['numpy', 'turtle']
+)
